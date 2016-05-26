ebf1c1074891e95a535056b0f1a76a44071f4f89
Valentina Akhmolina <valentina.ahmolina@yandex.ru>
2016-05-26 22:28:58 +0300

    Initial

---
+++ RightTriangle/ShapeCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RightTriangleShape
+{
+    public static class ShapeCalculator
+    {
+        public static double CalculateRightTriangleShape (double a, double b, double c)
+        {
+            if (a <= 0) { throw new ArgumentException("The a side is less then zero", "a"); }
+            if (b <= 0) { throw new ArgumentException("The b side is less then zero", "b"); }
+            if (c <= 0) { throw new ArgumentException("The c side is less then zero", "c"); }
+
+            double hip = Math.Max(a, b);
+            double cath1 = Math.Min(a, b);
+            double cath2;
+
+            if (hip == Math.Max(c, hip))
+            { cath2 = c; }
+            else
+            { cath2 = hip;  hip = c; }
+
+            if ( Math.Pow(hip,2) != Math.Pow(cath1, 2) + Math.Pow(cath2, 2))
+            { throw new Exception("Triangle is not right"); }
+
+            return cath1 * cath2;
+        }
+
+    }
+}
---
+++ ShapeCalculatorTest/ShapeCalculatorTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RightTriangleShape
+{
+    [TestClass]
+    public class ShapeCalculatorTests
+    {
+        public const string NotRightTriangelExceptionMessage = "Triangle is not right";
+
+        [TestMethod]
+        public void CalculateRightTriangleShape_RightTriangle_ReturnsShape()
+        {
+            // arrange
+            double a = 4;
+            double b = 3;
+            double c = 5;
+            double rightshape = 12;
+
+            // act
+            double shape = ShapeCalculator.CalculateRightTriangleShape(a,b,c);
+
+            // assert
+            Assert.AreEqual(rightshape, shape, 0.001 , "The fight triangle shape not calculaned correctly.");
+        }
+
+        [TestMethod]
+        public void CalculateRightTriangleShape_LessThenZeroSide_ThrowsArgumentException()
+        {
+            // arrange
+            double a = 4;
+            double b = -3;
+            double c = 5;
+
+            // act
+            try
+            {
+                double shape = ShapeCalculator.CalculateRightTriangleShape(a, b, c);
+            }
+            // assert
+            catch (ArgumentException ex)
+            {
+                Assert.AreEqual(ex.ParamName, "b");
+                return;
+            }
+            Assert.Fail("No exception was thrown.");
+        }
+
+        [TestMethod]
+        public void CalculateRightTriangleShape_NotRightTriangle_ThrowsException()
+        {
+            // arrange
+            double a = 5;
+            double b = 3;
+            double c = 5;
+
+            // act
+            try
+            {
+                double shape = ShapeCalculator.CalculateRightTriangleShape(a, b, c);
+            }
+            // assert
+            catch (Exception ex)
+            {
+                StringAssert.Contains(ex.Message, NotRightTriangelExceptionMessage);
+                return;
+            }
+            Assert.Fail("No exception was thrown.");
+        }
+    }
+}
